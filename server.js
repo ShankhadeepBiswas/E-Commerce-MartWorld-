5114ac7eb8398596ba4aaeec583325427d9b1fd2
Shankhadeep Biswas <sankhadeep.genx@gmail.com>
2020-11-16 06:10:39 +0530

    Express App setup with static and dynamic elements(ejs)

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
+const port = process.env.PORT || 3000
 const express = require('express')
 const app = express()
+const ejs = require('ejs')
+const path = require('path')
+
+//Middleware
+app.use('/public',express.static('public'))              //Static files
+
+app.set('view engine','ejs')
+
+app
+.get('/home',(req,res)=>{
+    res.render('home')
+})
+
+
+
+.get('*',(req,res)=>{
+    res.status(404).render('error')
+})
+
+.listen(port,()=>{
+    console.log('Server is up on port',port);
+})
